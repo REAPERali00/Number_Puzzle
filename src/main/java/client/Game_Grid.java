adb0a0cf0b7edef9bc58e12eb5f8278e1b128755
Alireza Jadidi <alireza2000jadidi@gmail.com>
2024-02-23 12:11:26 -0500

    Styling fixed, removed special buttons border and added hovers

--- src/main/java/client/Game_Grid.java
+++ src/main/java/client/Game_Grid.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import javafx.animation.PauseTransition;
 import javafx.animation.SequentialTransition;
 import javafx.animation.TranslateTransition;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
