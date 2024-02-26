0b186ff44156888a357d9b00ee9e2c474cb42739
Alireza Jadidi <alireza2000jadidi@gmail.com>
2024-02-26 14:18:29 -0500

    finished the basic router set up

--- server/routers/numpuz.js
+++ server/routers/numpuz.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
+router.get("/", (req, res) => {
+  res.send("Hi! welcome to num puz server!");
+});
+
 router
   .get("/ranking", (req, res) => {
     res.send("Testing router");
