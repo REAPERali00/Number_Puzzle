969262e67ede736dbe655c9f0d463c8e4c0c64f5
Alireza Jadidi <alireza2000jadidi@gmail.com>
2024-03-04 11:18:58 -0500

    small server update, added cors and .env file

--- server/index.js
+++ server/index.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 const app = express();
+
 app.use(express.json());
+app.use(cors());
+
 const NumRouter = require("./routers/numpuz");
-let port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use("/numpuz", NumRouter);
 app.get("/", (req, res) => res.send("accessing the server..."));
