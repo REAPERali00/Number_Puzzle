407f77067f065978ac454ea39778d3d50d61fcc7
Alireza Jadidi <alireza2000jadidi@gmail.com>
2024-03-04 13:30:16 -0500

    added okhtml to pom and module

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module client {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.media;
+    requires okhttp3;
 
     opens client to javafx.fxml;
 
--- src/main/java/server_connection/Api.java
+++ src/main/java/server_connection/Api.java
@@ -6,24 +6,12 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import okhttp3.*;
+
 public class Api implements ConfigApi {
-    private URL url;
-    private HttpURLConnection connection;
-    private BufferedInputStream in;
+    // private final OkHttpClient client = new OkHttpClient();
 
     public Api() {
 
-    }
-
-    public boolean connectUrl() {
-        try {
-            url = new URL(URL_PATH);
-            connection = (HttpURLConnection) url.openConnection();
-            return true;
-        } catch (IOException e) {
-            System.out.println("Couldn't connect to the url");
-            e.printStackTrace();
-            return false;
-        }
     }
 }
