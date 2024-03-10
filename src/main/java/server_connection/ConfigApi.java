089827b689e067bc20184fccc873d56afbff0d13
Alireza Jadidi <120317445+REAPERali00@users.noreply.github.com>
2024-03-10 13:51:32 -0400

    updating server to add to vercel

--- src/main/java/server_connection/ConfigApi.java
+++ src/main/java/server_connection/ConfigApi.java
@@ -1,7 +1,7 @@
 package server_connection;
 
 public interface ConfigApi {
-    String URL_PATH = "http://localhost:8000";
+    String URL_PATH = "https://number-puzzle-phi.vercel.app";
     String RANKS = "/numpuz/rankings";
     String POST_RANKS = "/numpuz/submitRank";
 }
