096b8c688d921560341a38d579bc6d5f7131e9dd
Alireza Jadidi <120317445+REAPERali00@users.noreply.github.com>
2024-03-10 14:08:16 -0400

    vercel

--- src/main/java/server_connection/ConfigApi.java
+++ src/main/java/server_connection/ConfigApi.java
@@ -1,7 +1,7 @@
 package server_connection;
 
 public interface ConfigApi {
-    String URL_PATH = "https://number-puzzle-phi.vercel.app";
+    String URL_PATH = "https://number-puzzle-phi.vercel.app/";
     String RANKS = "/numpuz/rankings";
     String POST_RANKS = "/numpuz/submitRank";
 }
