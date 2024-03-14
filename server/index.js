bd5a7dd3d9d2a7b90e6f4c5d7075ee319bc069d5
Alireza Jadidi <120317445+REAPERali00@users.noreply.github.com>
2024-03-14 09:32:26 -0400

    fixed landing page

--- server/index.js
+++ server/index.js
@@ -5,13 +5,14 @@ require("dotenv").config();
 const app = express();
 
 app.use(express.json());
+app.use(express.static("public"));
 app.use(cors());
 const NumRouter = require("./routers/numpuz");
 const port = process.env.PORT || 3000;
 
 app.use("/numpuz", NumRouter);
 app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "frontpage/index.html"))
+  res.sendFile(path.join(__dirname, "public", "frontpage/index.html"))
 );
 app.listen(port, () => {
   console.log(`Server is listening on ${port}`);
