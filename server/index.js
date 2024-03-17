88de5f179f84030dfc822f59d0f7f2f07a9b0d26
REAPERali00 <alireza2000jadidi@gmail.com>
2024-03-17 17:03:00 -0400

    updated the pulic site

--- server/index.js
+++ server/index.js
@@ -5,15 +5,16 @@ require("dotenv").config();
 const app = express();
 
 app.use(express.json());
-app.use(express.static("public"));
 app.use(cors());
+app.use(express.static(path.join(__dirname, "/public")));
+
 const NumRouter = require("./routers/numpuz");
 const port = process.env.PORT || 3000;
 
 app.use("/numpuz", NumRouter);
-app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "public", "index.html"))
-);
+// app.get("/", (req, res) =>
+//   res.sendFile(path.join(__dirname, "public", "index.html"))
+// );
 app.listen(port, () => {
   console.log(`Server is listening on ${port}`);
 });
